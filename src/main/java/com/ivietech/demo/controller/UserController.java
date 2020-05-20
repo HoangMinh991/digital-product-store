6f55c0e4b8d693eaa197af2741bef0f7cfb19904
HoangMinh991 <hoangminh.dlv@gmail.com>
2020-05-21 01:48:09 +0700

    Add userInfo

---
+++ src/main/java/com/ivietech/demo/controller/UserController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ivietech.demo.controller;
+
+import com.ivietech.demo.dao.BalanceRepository;
+import com.ivietech.demo.dao.OrderRepository;
+import com.ivietech.demo.dao.PlaformRepository;
+import com.ivietech.demo.dao.ProductRepository;
+import com.ivietech.demo.dao.TypeRepository;
+import com.ivietech.demo.dao.UserRepository;
+import com.ivietech.demo.entity.Platforms;
+import com.ivietech.demo.entity.Type;
+import com.ivietech.demo.entity.User;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author HoangMinh
+ */
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private BalanceRepository balanceRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private TypeRepository typeRepository;
+    @Autowired
+    private PlaformRepository plaformRepository;
+
+    @GetMapping("/user/info")
+    @PreAuthorize("hasRole('READ_PRIVILEGE')")
+    public String userInfo(Model model) {
+        List<Platforms> listPlatforms = plaformRepository.findAll();
+        List<Type> listType = typeRepository.findAll();
+        model.addAttribute("listPlatforms", listPlatforms);
+        model.addAttribute("listType", listType);
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUserName(userName);
+        model.addAttribute("user", user);
+        model.addAttribute("value", user.getBalance().getMoney());
+        model.addAttribute("title", "Thông tin tài khoản");
+        return "/user/userInfo1";
+    }
+
+}
