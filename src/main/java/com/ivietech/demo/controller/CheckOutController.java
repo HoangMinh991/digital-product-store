6eefa83153d6d49da41ee62f1cd7fe24681821b4
phuoctam3009 <58617141+phuoctam3009@users.noreply.github.com>
2020-05-20 17:32:00 +0700

    complete cart detail

---
+++ src/main/java/com/ivietech/demo/controller/CheckOutController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ivietech.demo.controller;
+
+import com.ivietech.demo.dao.BalanceRepository;
+import com.ivietech.demo.dao.OrderRepository;
+import com.ivietech.demo.dao.PlaformRepository;
+import com.ivietech.demo.dao.ProductRepository;
+import com.ivietech.demo.dao.TypeRepository;
+import com.ivietech.demo.dao.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author ADMIN
+ */
+@Controller
+public class CheckOutController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private BalanceRepository balanceRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private TypeRepository typeRepository;
+    @Autowired
+    private PlaformRepository plaformRepository;
+    
+    @RequestMapping("/checkout")
+    public String test(Model model){
+        System.out.println("abcd");
+        return "user/abc";
+    }
+}
--- src/main/java/com/ivietech/demo/dto/UserDto.java
+++ src/main/java/com/ivietech/demo/dto/UserDto.java
@@ -5,6 +5,12 @@
  */
 package com.ivietech.demo.dto;
 
+import com.ivietech.demo.entity.Balance;
+import com.ivietech.demo.entity.Orders;
+import com.ivietech.demo.entity.Role;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +21,15 @@ import org.springframework.stereotype.Component;
 @Data
 public class UserDto {
 
-    private String name;
+    private long id;
     private String userName;
+    private String name;
     private String password;
-    private String passwordConfirm;
+    private boolean enabled;
     private String email;
-    private String phone;   
+    private String phone;
+    private Date createdDatetime;
+    private Set<Role> roles;
+    Balance balance;
+    private List<Orders> listOrder;
 }
